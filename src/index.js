6e0588214826b975ab7d5937ddf2f1888f0ceda9
Yury Shapkarin <yury@shapkarin.me>
2019-08-06 22:55:44 +0300

    move static names to array

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 var reactRedux = require('react-redux');
 
+var staticNames = [
+  'mapStateToProps',
+  'mapDispatchToProps',
+  'mergeProps',
+  'connectOptions'
+];
+
 module.exports = function connector(Component) {
-  return reactRedux.connect(Component.mapStateToProps, Component.mapDispatchToProps, Component.mergeProps, Component.connectOptions)(Component);
+  return reactRedux.connect.apply(null, staticNames.map(function(name){
+    return Component[name]
+  }))(Component);
 };
